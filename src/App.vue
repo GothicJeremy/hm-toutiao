5ca32efbdfe242df32578769261804cba3ed8944
Your Name <email@example.com>
2019-07-12 16:02:05 +0800

    使用vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     APP 根组件
+    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
 <script>
 
-
 export default {
-  name: 'app',
+  name: 'app'
 }
 </script>
 
