bb56c8161ca00955983f9de2bb832216604e9b52
zhousg <zhoushugang@itcast.cn>
2019-07-14 12:09:38 +0800

    插槽使用

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -44,12 +44,19 @@
       </el-form>
     </el-card>
     <!-- 结果容器 -->
-    <el-card></el-card>
+    <el-card>
+      <my-test>
+        <template v-slot:con="scope">内容2 {{scope.test}}</template>
+        <template slot="fot">底部2</template>
+      </my-test>
+    </el-card>
   </div>
 </template>
 
 <script>
+import MyTest from '@/components/my-test.vue'
 export default {
+  components: { MyTest },
   data () {
     return {
       // 提交给后台的筛选条件  传参
